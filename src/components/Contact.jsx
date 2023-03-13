e57fe052c2bbb512e8186fb04c7f5ab352bc3399
Antonio Lucas <61833383+MisteriumEz@users.noreply.github.com>
2023-03-12 21:06:59 -0300

    feat(all): Adição da base do projeto, necessitando finalizar alterações

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,9 +1,127 @@
-import React from 'react'
+import { useState, useRef } from "react";
+import { motion } from "framer-motion";
+import emailjs from "@emailjs/browser";
+
+import { styles } from "../styles";
+import { EarthCanvas } from "./canvas";
+import { SectionWrapper } from "../hoc";
+import { slideIn } from "../util/motion";
 
 const Contact = () => {
-  return (
-    <div>Contact</div>
-  )
-}
+  const formRef = useRef();
+  const [form, setForm] = useState({
+    name: "",
+    email: "",
+    message: "",
+  });
+  const [loading, setLoading] = useState(false);
 
-export default Contact
+  const handleChange = (e) => {
+    setForm({ ...form, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setLoading(true);
+
+    emailjs
+      .send(
+        "service_1j8qj8j",
+        "template_1j8qj8j",
+        {
+          from_name: form.name,
+          from_email: form.email,
+          to_name: "Big Tony",
+          to_email: "bigtony.dev@gmail.com",
+          message: form.message,
+        },
+        "user_1j8qj8j"
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+          setLoading(false);
+          alert("Thanks for contacting me! I'll get back to you soon.");
+
+          setForm({
+            name: "",
+            email: "",
+            message: "",
+          });
+        },
+        (error) => {
+          console.log(error.text);
+          setLoading(false);
+          alert("Something went wrong. Please try again later.");
+        }
+      );
+  };
+
+  return (
+    <div className="xl:mt-12 xl:flex-row flex-col-reverse flex gap-10 overflow-hidden">
+      <motion.div
+        variants={slideIn("left", "tween", 0.2, 1)}
+        className="flex-[0.75px] bg-black-100 p-8 rounded-2xl"
+      >
+        <p className={styles.sectionSubText}> Get in touch</p>
+        <h3 className={styles.sectionHeadText}>Contact</h3>
+        <form
+          ref={formRef}
+          onSubmit={handleSubmit}
+          className="mt-12 flex flex-col gap-8"
+        >
+          <label className="flex flex-col">
+            <span className="text-white font-medium mb-4 ">Your Name</span>
+            <input
+              type="text"
+              name="name"
+              value={form.name}
+              onChange={handleChange}
+              placeholder="What's your name?"
+              className="bg-tertiary py-4 px-6 placeholder:text-secondary text-white rounded-lg outline-none border-none font-medium"
+            />
+          </label>
+
+          <label className="flex flex-col">
+            <span className="text-white font-medium mb-4 ">Your Email</span>
+            <input
+              type="email"
+              name="email"
+              value={form.email}
+              onChange={handleChange}
+              placeholder="What's your email?"
+              className="bg-tertiary py-4 px-6 placeholder:text-secondary text-white rounded-lg outline-none border-none font-medium"
+            />
+          </label>
+
+          <label className="flex flex-col">
+            <span className="text-white font-medium mb-4 ">Your Message</span>
+            <textarea
+              rows="7"
+              name="message"
+              value={form.message}
+              onChange={handleChange}
+              placeholder="What do you want to say?"
+              className="bg-tertiary py-4 px-6 placeholder:text-secondary text-white rounded-lg outline-none border-none font-medium"
+            />
+          </label>
+          <button
+            type="submit"
+            className="bg-tertiary py-3 px-8 outline-none w-fit text-white font-bold shadow-md shadow-primary rounded-xl "
+          >
+            {loading ? "Sending..." : "Send"}
+          </button>
+        </form>
+      </motion.div>
+
+      <motion.div
+        variants={slideIn("right", "tween", 0.2, 1)}
+        className="xl:flex-1 xl:h-auto md:h-[550px] h-[350px]"
+      >
+        <EarthCanvas />
+      </motion.div>
+    </div>
+  );
+};
+
+export default SectionWrapper(Contact, "contact");
